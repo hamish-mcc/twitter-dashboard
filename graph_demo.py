2ce702c779f35eb4ddffb0a3de3095de02ed9032
hamish-mcc <hamishmcc24@gmail.com>
2021-05-17 11:06:55 +1200

    inital demo code

---
+++ graph_demo.py
@@ -0,0 +1,101 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+import dash_cytoscape as cyto
+from tweepy_helper import get_tweets
+import pandas as pd
+from dash.dependencies import Input, Output, State
+from plotly.offline import init_notebook_mode
+
+init_notebook_mode(connected=True)
+
+# Explore external_stylesheets themes here https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/explorer/
+app = dash.Dash(external_stylesheets=[dbc.themes.SOLAR])
+
+# Define app layout, using Dash Bootstrap Components https://dash-bootstrap-components.opensource.faculty.ai/docs/
+app.layout = dbc.Container(
+    [
+        html.H1("graph_demo.py"),
+        dbc.Row([
+            dbc.Col(dbc.FormGroup(
+                [
+                    dbc.Label("Search Phrase", html_for="search_phrase"),
+                    dbc.Input(
+                        id="search_phrase", placeholder="Enter a search phrase...", type="text"),
+                    dbc.Button("Update", id="update", color="primary", block=True)
+                ]
+            ), md=2),
+            dbc.Col(
+                cyto.Cytoscape(
+                    id='cytoscape',
+                    layout={'name': 'cose'},
+                    style={'width': '100%', 'height': '800px'},
+                    elements=[],
+                    stylesheet=[
+                        # See https://js.cytoscape.org/#style
+                        # Colour nodes by sentiment, size by support
+                        {
+                            'selector': 'node',
+                            'style': {
+                                'label': 'data(label)',
+                                'color': 'white',
+                                'font-size': 6,
+                                'background-color': 'mapData(weight, -1, 0, red, yellow)',
+                                "width": 'data(size)',
+                                "height": 'data(size)',
+                                "opacity": 0.75
+                            },
+                        }, {
+                            'selector': 'edge',
+                            'style': {
+                                'width': 0.5
+                            }
+                        },
+                    ]
+                ), md=10)]),
+    ],
+    fluid=True,
+)
+
+
+@app.callback(
+    # Callback to update graph elements, based on a search phrase
+    Output("cytoscape", "elements"),
+    Input("update", "n_clicks"),
+    State("search_phrase", "value"), prevent_initial_call=True)
+def update_graph(search_phrase, update):
+    # User defines number of tweets and search string
+    node_df, edge_df = get_tweets(search_phrase, 1000)
+
+    # Use local data frames for development purposes
+    #node_df = pd.read_csv(
+        #"C:/Users/hamis/Documents/Uni/158222/assignments/assignment 3/development/hashtag_sentiment.csv", index_col=0)
+    #edge_df = pd.read_csv(
+        #"C:/Users/hamis/Documents/Uni/158222/assignments/assignment 3/development/hashtag_pairing.csv", index_col=0)
+
+    elements = []
+    counts = node_df.groupby(by='tag').count()
+    avg_sentiment = node_df.groupby(by='tag').mean()
+    # Add nodes
+    for tag in node_df['tag'].unique():
+        node = {
+            'data': {'id': tag,
+                     'label': tag,
+                     # A scaling factor is applied to each node here.
+                     'size': int(counts.loc[tag]) * 3,
+                     'weight': float(avg_sentiment.loc[tag])
+                     }
+        }
+        elements.append(node)
+    # Add edges
+    for pair in edge_df.index:
+        edge = {'data': {
+            'source': edge_df.loc[pair, 'tag'], 'target': edge_df.loc[pair, 'associated_tag']}}
+        if edge not in elements:
+            elements.append(edge)
+
+    return elements
+
+
+if __name__ == "__main__":
+    app.run_server()
---
+++ tweepy_helper.py
@@ -0,0 +1,48 @@
+import tweepy as tw
+import pandas as pd
+from textblob import TextBlob
+import itertools
+import json
+
+# Load keys from configuration
+with open("credentials.json") as credentials:
+    keys = json.load(credentials)
+
+consumer_key = keys["api_key"]
+consumer_secret = keys["api_secret_key"]
+access_token = keys["access_token"]
+access_token_secret = keys["access_token_secret"]
+
+print("Error loading configuration from 'credentials.json'...")
+
+# Complete OAUTH with API
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tw.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+# Gets a number of tweets and returns dataframes for nodes (hashtags) and edges (hashtag pairs)
+def get_tweets(search_string, n_items):
+    node_df = pd.DataFrame(columns=["tag", "sentiment"])
+    edge_df = pd.DataFrame(columns=["tag", "associated_tag"])
+
+    tweets = tw.Cursor(api.search, count=1000, q=search_string,
+                              show_user=True, tweet_mode="extended").items(n_items)
+
+    for tweet in tweets:
+        try:
+            temp_tags = []
+            for _, tag in enumerate(tweet.entities.get('hashtags')):
+                temp_tags.append(tag["text"])
+                node_df = node_df.append({"tag": tag["text"], "sentiment": TextBlob(
+                    tweet.full_text).polarity}, ignore_index=True)
+            res = list(itertools.combinations(temp_tags, 2))
+            if res != []:
+                for pair in res:
+                    edge_df = edge_df.append(
+                        {"tag": pair[0], "associated_tag": pair[1]}, ignore_index=True)
+        except:
+            pass
+
+    return node_df, edge_df
